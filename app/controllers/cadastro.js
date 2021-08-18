90b0ae44cd8ab24f2b14a86a0c75a4834a35c6e0
Elton <eltonsa75@hotmail.com>
2021-08-18 19:50:32 -0300

    Fazendo conexão com o Banco MongoDB e cadastrando usuario.

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,5 +1,5 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro', {validacao: {}, dadosForm: {} });
+    res.render('cadastro', { validacao: {}, dadosForm: {} });
 }
 
 module.exports.cadastrar = function(application, req, res){
@@ -8,15 +8,21 @@ module.exports.cadastrar = function(application, req, res){
 
     req.assert('nome', 'Nome não pode ser vazio').notEmpty();
     req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
-    req.assert('senha', 'Senha não pode ser vazio').notEmpty();
-    req.assert('casa', 'Casa não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+    req.assert('casa', 'Casa não pode ser vazia').notEmpty();
 
     var erros = req.validationErrors();
 
     if(erros){
-       res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+        res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
         return;
     }
 
-    res.send('podemos cadastrar')
+    var connection = application.config.dbConnection;
+    var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+
+    UsuariosDAO.inserirUsuario(dadosForm);
+
+    res.send('podemos cadastrar');
+
 }
