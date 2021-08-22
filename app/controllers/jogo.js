b7ec84e5a1dedbb65c8a786a738b47e186f99596
Elton <eltonsa75@hotmail.com>
2021-08-21 21:09:20 -0300

    Jquery dos Botões suditos e pergaminhos

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -18,5 +18,13 @@ module.exports.jogo = function(application, req, res){
 module.exports.sair = function(application, req, res){
    req.session.destroy( function(err){
        res.render("index", {validacao: {}});
-   })
+   });
+}
+
+module.exports.suditos = function(application, req, res){
+    res.render("aldeoes", {validacao: {}});
+}
+
+module.exports.pergaminhos = function(application, req, res){
+    res.render("pergaminhos", {validacao: {}});
 }
